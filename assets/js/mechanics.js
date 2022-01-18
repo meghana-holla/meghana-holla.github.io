a03a66a69412ea46fc0f0ba057e8fc5d2c495b96
Meghana Holla <meghana.holla@gmail.com>
2022-01-18 18:32:11 -0500

    cleaned .js

--- assets/js/mechanics.js
+++ assets/js/mechanics.js
@@ -1,70 +1,8 @@
-// // get the element to animate
-// var element = document.querySelector("content-item");//.getElementById('box');
-
-// var navItems = document.querySelector("nav-item");
-
-
-
-// var elementHeight = element.clientHeight;
-
-// // listen for scroll event and call animate function
-// document.addEventListener('scroll', animate);
-
-// // check if element is in view
-// function inView() {
-//   // get window height
-//   var windowHeight = window.innerHeight;
-//   // get number of pixels that the document is scrolled
-//   var scrollY = window.scrollY || window.pageYOffset;
-  
-//   // get current scroll position (distance from the top of the page to the bottom of the current viewport)
-//   var scrollPosition = scrollY + windowHeight;
-//   // get element position (distance from the top of the page to the bottom of the element)
-//   var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight;
-  
-//   // is scroll position greater than element position? (is element in view?)
-//   if (scrollPosition > elementPosition) {
-//     return true;
-//   }
-  
-//   return false;
-// }
-
-// // animate element when it is in view
-// function animate() {
-//   // is element in view?
-//   if (inView()) {
-//       // element is in view, add class to element
-//       element.classList.add('animate');
-//   }
-// }
-
-var publications = [
-  {
-      "title": "Dialog Driven Face Construction using GANs",
-      "authors": "Malaika Vijay, Meghana, Nishant Aklecha, Ramamoorthy Srinath",
-      "at":"ICTAI 2020",
-      "year": 2020,
-      "doi": "https://doi.org/10.1109/ICTAI50040.2020.00104",
-      "pdf": "#"
-  },
-  {
-      "title": "Detection of Emphasis Words in Short Texts – A Context Aware Label Distribution Learning Approach",
-      "authors": "Meghana, Bhaskarjyoti Das",
-      "at":"ICAICR 2020",
-      "year": 2021,
-      "doi": "https://doi.org/10.1007/978-981-16-3660-8_32",
-      "pdf": "#"
-  }
-]
-
 window.addEventListener('DOMContentLoaded', () => {
     console.log("Ji");
 
     function toggleHide() {
       var x = document.querySelector(".nav-items-list");
-      // if (x.style.display !== "none") {
-          // x.style.display = "none";
       x.classList.toggle("items-list-hidden")
     }
 
@@ -72,18 +10,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById("burger").addEventListener("click", function() {
       toggleHide();
-        // var x = document.querySelector(".nav-items-list");
-        // if (x.style.display !== "none") {
-            // x.style.display = "none";
-            // x.classList.toggle("items-list-hidden")
-            // c.classList.remove("fa-chevron-up")
-            // c.classList.add("fa-chevron-down")
-        //   } else {
-            // x.style.display = "block";
-            // x.classList.remove("items-list-hidden")
-            // c.classList.remove("fa-chevron-down")
-            // c.classList.add("fa-chevron-up")
-        //   }
     })
 
     if(window.matchMedia("(max-width: 600px)").matches) {
@@ -103,7 +29,7 @@ window.addEventListener('DOMContentLoaded', () => {
       }, {threshold: [0,0.05,0.1]});
     
       console.group(document.querySelectorAll('.name-section'))
-      // Track all sections that have an `id` applied
+      
       document.querySelectorAll('.name-section').forEach((section) => {
         observer.observe(section);
       })
@@ -115,45 +41,6 @@ window.addEventListener('DOMContentLoaded', () => {
         })
       })
     }
-
-    
-
-    // var c = document.querySelector("#chev");
-
-    // function myFunction() {
-    //     var x = document.querySelector(".nav-items-list");
-        
-    //     if (x.style.display !== "none") {
-    //       x.style.display = "none";
-    //       c.classList.remove("fa-chevron-up")
-    //       c.classList.add("fa-chevron-down")
-    //     } else {
-    //       x.style.display = "block";
-    //       c.classList.remove("fa-chevron-down")
-    //       c.classList.add("fa-chevron-up")
-    //     }
-    //   }
-    // if(window.matchMedia("(max-width: 600px)").matches) {
-    //     var wrap = document.querySelector(".header");
-    //     console.log(wrap)
-    //     // document.addEventListener("scroll", function(e) {
-    //     //     console.log(wrap.scrollY)
-    //     //     if (this.scrollTop > 200) {
-    //     //         alert("Hey")
-    //     //         c.style.display = "block";
-    //     //       // wrap.classList.add("fix-search");
-    //     //     } else {
-    //     //         c.style.display = "none";
-    //     //       // wrap.classList.remove("fix-search");
-    //     //     }
-            
-    //     //   });
-
-    //     document.querySelector(".nav-items-list").addEventListener("click", function(e) {
-    //         myFunction();
-    //     })
-    // }
-    
 
 
     //const observer_mobile
@@ -174,7 +61,7 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }, {threshold: [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0], root: document.querySelector("new-content")});
   
-    // Track all sections that have an `id` applied
+    
     document.querySelectorAll('.content-item[id]').forEach((section) => {
       observer.observe(section);
     })
